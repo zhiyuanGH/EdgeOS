fe445aba4dabda9ee21bf6769af9a50586d834da
stevenzhang124 <1241922639@qq.com>
2021-10-18 15:07:08 +0800

    update1018

--- Master/Scheduler.py
+++ Master/Scheduler.py
@@ -1,7 +1,6 @@
 
 
-
-#profile the application (latency, energy consumption, .etc)
+#profile the application (latency, energy consumption, .etc), can be done offline
 
 #get network topology and bandwidth
 
@@ -9,4 +8,33 @@
 
 #user customized algorithms
 
-#dynamically generate the execution file
+#dynamically generate the execution file and policies
+#execution configuration
+conf = 	{
+			"sub_task_1": {
+				"task_id": "0 1 2", 
+				"source_ip": "192.168.1.104", 
+				"source_ip_port": 8089, 
+				"next_hop": "192.168.1.104", 
+				"next_hop_port": 8089, 
+				"bandwidth": "", 
+				"routing": ""},
+
+			"sub_task_2": {
+				"task_id": "0 1 2", 
+				"source_ip": "192.168.1.104", 
+				"source_ip_port": 8089, 
+				"next_hop": "192.168.1.102", 
+				"next_hop_port": 8089, 
+				"bandwidth": "", 
+				"routing": ""},
+
+			"sub_task_3": {
+				"task_id": "0 1 2", 
+				"source_ip": "192.168.1.104", 
+				"source_ip_port": 8089, 
+				"next_hop": "192.168.1.104", 
+				"next_hop_port": 8089, 
+				"bandwidth": "", 
+				"routing": ""}
+		}
